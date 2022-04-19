30d892e30014a2c0789001c84e22ca2df0a0f4f1
Kocmonavtik <chapligin141299@gmail.com>
2022-04-19 07:14:46 -0700

    Создание таблиц и миграций

---
+++ src/Entity/Lesson.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LessonRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=LessonRepository::class)
+ */
+class Lesson
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Course::class, inversedBy="lessons")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $course_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\Range(min="0", max="10000", notInRangeMessage="No more than 10000 lessons")
+     */
+    private $serial_number;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCourseId(): ?Course
+    {
+        return $this->course_id;
+    }
+
+    public function setCourseId(?Course $course_id): self
+    {
+        $this->course_id = $course_id;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSerialNumber(): ?int
+    {
+        return $this->serial_number;
+    }
+
+    public function setSerialNumber(int $serial_number): self
+    {
+        $this->serial_number = $serial_number;
+
+        return $this;
+    }
+}
