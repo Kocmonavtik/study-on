01a38e7d2b46bdc043c57d8cca048684239105bc
Kocmonavtik <chapligin141299@gmail.com>
2022-04-20 01:23:43 -0700

    Переименованы роуты, назначена главная страница, вывод курсов и их уроков

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/course")
+ * @Route("/courses")
  */
 class CourseController extends AbstractController
 {
